a538087a1d04a0e9b5a7c94dd2c055ba72963863
Ilia Bulkin <bulkin20088@gmail.com>
2024-09-22 12:42:43 +0300

    homework js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express()
+const path = require('path')
 
+app.use('/static/', express.static(path.join(__dirname, 'static')))
 
 function getCurrentDate() {
     return new Date().toLocaleString(); 
 }
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./templates/index.html"))
+})
 
 app.get('/date', (req, res) => {
     const currentDate = getCurrentDate();
