58956546d2b7397d7f2d011e119737e3feab27e8
Ilia Bulkin <bulkin20088@gmail.com>
2024-09-28 22:51:20 +0300

    new branch Comments and Comments

--- src/server.js
+++ src/server.js
@@ -17,12 +17,12 @@ function getCurrentDate() {
 app.get('/', (req, res) => {
     // res.sendFile(path.resolve(__dirname, "./templates/index.ejs"))
     const context = {
-        title: 'aboba2'
+        title: 'aboba'
      }
     res.render('index', context)
 })
 
-app.get('/date', (req, res) => {
+app.get('/date/', (req, res) => {
     const currentDate = getCurrentDate();
     console.log(currentDate); 
     res.send(`${currentDate}`); 
@@ -41,6 +41,15 @@ app.get('/posts', (req, res) => {
 
 app.get('/user/', (req, res) => {
     res.render('user', cache)
+})
+
+app.get('/comments/', (req, res) => {
+    const context = {
+        comments: [{title: 'title1', message: 'aboba1', author: 'author1', authorImg: '/static/authorImg1.jpg'}, 
+                    {title: 'title2', message: 'aboba2', author: 'author2', authorImg: '/static/authorImg1.jpg'},
+                    {title: 'title3', message: 'aboba3', author: 'author3', authorImg: '/static/authorImg1.jpg'},]
+    }
+    res.render('comments', context)
 })
 
 const PORT = 8000;
