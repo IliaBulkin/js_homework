22cd84fa5b812e6c3cba3c39b72dd75eb448b316
Ilia Bulkin <bulkin20088@gmail.com>
2024-10-01 01:28:18 +0300

    homework

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ function getCurrentDate() {
     return new Date().toLocaleString(); 
 }
 
+
 app.get('/', (req, res) => {
     // res.sendFile(path.resolve(__dirname, "./templates/index.ejs"))
     const context = {
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
      }
     res.render('index', context)
 })
+
 
 app.get('/date/', (req, res) => {
     const currentDate = getCurrentDate();
@@ -39,9 +41,11 @@ app.get('/posts', (req, res) => {
     res.render('posts', context)
 })
 
+
 app.get('/user/', (req, res) => {
     res.render('user', cache)
 })
+
 
 app.get('/comments/', (req, res) => {
     const context = {
@@ -51,6 +55,71 @@ app.get('/comments/', (req, res) => {
     }
     res.render('comments', context)
 })
+
+
+const post_for_page = [
+    {
+        "name": "a1",
+        "description": "a1 description",
+        "date": "a1 date",
+        "author": "a1 author"
+    },
+    {
+        "name": "b2",
+        "description": "b2 description",
+        "date": "b2 date",
+        "author": "b2 author"
+    },
+    {
+        "name": "c3",
+        "description": "c3 description",
+        "date": "c3 date",
+        "author": "c3 author"
+    },
+    {
+        "name": "d4",
+        "description": "d4 description",
+        "date": "d4 date",
+        "author": "d4 author"
+    },
+    {
+        "name": "e5",
+        "description": "e5 description",
+        "date": "e5 date",
+        "author": "e5 author"
+    }
+]
+
+
+// Через ? задаем текст для поиска, как в браузере
+app.get('/postss', (req, res) => {
+    const context = {
+        post: post_for_page
+    }
+    console.log(req.query)
+    const max = req.query.max
+    if (max <= post_for_page.length) {
+        context.post_for_page = post_for_page.slice(0, max)
+    }
+    res.render('postss', context)
+})
+
+app.get('/post/:id', (req, res) => {
+    console.log(req.params) // params - параметры(в данном случае id)
+    const context = {
+        post: post_for_page[req.params.id-1]
+    }
+    if (req.params.id <= post_for_page.length) {
+        res.render('post', context)
+    } else {
+        // Сережа, я не очень понял формулировку некоторых пунктов заданий
+        // Например "поменять направление расположения на вертикальное"
+        // И с доп заданием та же история, но вроде новый способ придумал, поэтому считаю этот пункт выполненным
+        res.render('error', cache)
+    }
+    
+})
+
 
 const PORT = 8000;
 app.listen(PORT, () => {
